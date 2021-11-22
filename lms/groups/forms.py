ff09323c0a19ed5bbb3ea85dcc6f7323b4197b32
Dima Popchenko <bonia.2150f@gmail.com>
2021-11-22 21:26:15 +0200

    Робота з полями в моделях

--- lms/groups/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm, DateInput
-from groups.models import Group
-
-
-class GroupCreateForm(ModelForm):
-    class Meta:
-        model = Group
-        fields = ["first_name", "last_name", "num_group"]
--- lms/groups/utils.py
+++
@@ -1,6 +0,0 @@
-def format_records(records):
-    if not records:
-        return "(Empty recordset)"
-    return "<br>".join(
-        f'<a href="/groups/update_groups/{rec.id}/">EDIT</a>{rec}' for rec in records
-    )
